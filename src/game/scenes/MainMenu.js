8475a4dc315b698a44a1350607e3dbae8ed8b9ff
Marcel Göhn <marcelgoehn@googlemail.com>
2026-04-18 22:03:27 +0200

    added basic button to the start scren

--- src/game/scenes/MainMenu.js
+++ src/game/scenes/MainMenu.js
@@ -1,20 +1,28 @@
 import { Scene } from 'phaser';
 
-export class MainMenu extends Scene
-{
-    constructor ()
-    {
+export class MainMenu extends Scene {
+    constructor() {
         super('MainMenu');
     }
 
-    create ()
-    {
+    create() {
         this.add.image(512, 384, 'background');
+        this.drawButton(100, 100, 'Start Game');
+        this.drawButton(300, 100, 'Impressum');
+    }
 
-        this.input.once('pointerdown', () => {
+    drawButton(x, y, text) {
+        this.startButton = this.add.text(x, y, text, { fill: '#0f0' });
+        this.startButton.setInteractive({ useHandCursor: true });
+        this.startButton.on('pointerup', () => this.scene.start('Game'));
+        this.startButton.on('pointerover', () => this.enterButtonHoverState());
+    }
 
-            this.scene.start('Game');
+    enterButtonHoverState() {
 
-        });
+    }
+
+    leaveButtonHoverState() {
+
     }
 }
