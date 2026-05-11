a31a36277bd7b443479b412fc37692615f54f041
Marcel Göhn <marcelgoehn@googlemail.com>
2026-05-11 21:48:23 +0200

    feat: added small chicken

--- src/game/animations/chicken-animations.js
+++ src/game/animations/chicken-animations.js
@@ -12,4 +12,18 @@ export default function createChickenAnimations(scene) {
         frameRate: 30,
         repeat: 10
     })
+
+    scene.anims.create({
+        key: 'small-chicken-walking',
+        frames: scene.anims.generateFrameNumbers('small-chicken', { start: 0, end: 2 }),
+        frameRate: 20,
+        repeat: -1
+    });
+
+    scene.anims.create({
+        key: 'small-chicken-dead',
+        frames: scene.anims.generateFrameNumbers('small-chicken', { start: 3, end: 3 }),
+        frameRate: 30,
+        repeat: 10
+    });
 }
--- src/game/collider/collisions.js
+++ src/game/collider/collisions.js
@@ -2,4 +2,6 @@ export default function createColliders(scene) {
     scene.physics.add.collider(scene.player, scene.platform);
     scene.physics.add.collider(scene.normalChicken, scene.platform);
     scene.physics.add.overlap(scene.player, scene.normalChicken, scene.killChicken, null, scene);
+    scene.physics.add.collider(scene.smallChicken, scene.platform);
+    scene.physics.add.overlap(scene.player, scene.smallChicken, scene.killChicken, null, scene);
 }
